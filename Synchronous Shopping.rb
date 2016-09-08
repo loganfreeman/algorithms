4b5fed630f2db55f59fc72e49df5c2304fda77ae
loganfreeman <shanhongcheng@hotmail.com>
2016-09-08 16:31:25 -0600

    Update Synchronous Shopping.rb

--- Synchronous Shopping.rb
+++ Synchronous Shopping.rb
@@ -76,6 +76,26 @@ class UnionFind
     end
 end
 
+
+class Kruskal
+  def compute_mst(graph)
+    mst = []
+    edges = graph.edges.sort!
+
+    union_find = UnionFind.new(graph.nodes)
+    while edges.any? && mst.size <= graph.nodes.size
+      edge = edges.shift
+      if !union_find.connected?(edge.node1, edge.node2)
+        union_find.union(edge.node1, edge.node2)
+        mst << edge
+      end
+    end
+
+    mst
+  end
+end
+
+
 N, M, K = gets.strip.split(' ').map(&:to_i)
 i = 1
 graph = Graph.new
