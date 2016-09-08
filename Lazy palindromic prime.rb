469c1631581423bf64ce4594362328573e5adfae
loganfreeman <shanhongcheng@hotmail.com>
2016-09-08 11:03:08 -0600

    Update Lazy palindromic prime.rb

--- Lazy palindromic prime.rb
+++ Lazy palindromic prime.rb
@@ -1,1 +1,34 @@
 
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+require 'prime'
+n = gets.strip.to_i
+
+def palindrome?(string)
+  if string.length == 1 || string.length == 0
+    true
+  else
+    if string[0] == string[-1]
+      palindrome?(string[1..-2])
+    else
+      false
+    end
+  end
+end
+
+palindromie_prime_array = -> (size) do
+    series = Prime.lazy.each
+    count = 0
+    result = []
+    while count < size
+        p = series.next
+        if palindrome?(p.to_s)
+            result.push(p)
+            count += 1
+        end
+    end
+    return result
+end
+
+
+
+puts palindromie_prime_array.(n).to_s
