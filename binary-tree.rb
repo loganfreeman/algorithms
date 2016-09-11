4d883046d748366b7d7286ccfd39ad03f520c7d6
loganfreeman <shanhongcheng@hotmail.com>
2016-09-11 10:04:06 -0600

    Update binary-tree.rb

--- binary-tree.rb
+++ binary-tree.rb
@@ -51,6 +51,67 @@ class Tree
             list << node.right unless node.right.nil?
         end
     end
+
+    def inorder
+        @left.inorder { |y| yield y } unless @left.nil?
+        yield @data
+        @right.inorder { |y| yield y } unless @right.nil?
+    end
+
+    def preorder
+        yield @data
+        @left.preorder { |y| yield y } unless @left.nil?
+        @right.preorder { |y| yield y } unless @right.nil?
+    end
+
+    def postorder
+        @left.postorder { |y| yield y } unless @left.nil?
+        @right.postorder { |y| yield y } unless @right.nil?
+        yield @data
+    end
+
+    def search(x)
+        if data == x
+            return self
+        else
+            ltree = !left.nil? ? left.search(x) : nil
+            return ltree unless ltree.nil?
+            rtree = !right.nil? ? right.search(x) : nil
+            return rtree unless rtree.nil?
+        end
+        nil
+    end
+
+    def to_s
+        '[' +
+            (left ? left.to_s + ',' : '') +
+            data.inspect +
+            (right ? ',' + right.to_s : '') + ']'
+    end
+
+    def to_a
+        temp = []
+        temp += left.to_a if left
+        temp << data
+        temp += right.to_a if right
+        temp
+    end
+
+    def infix
+        unless @left.nil?
+            flag = %w(* / + -).include? @left.data
+            yield '(' if flag
+            @left.infix { |y| yield y }
+            yield ')' if flag
+        end
+        yield @data
+        unless @right.nil?
+            flag = %w(* / + -).include? @right.data
+            yield '(' if flag
+            @right.infix { |y| yield y } unless @right.nil?
+            yield ')' if flag
+        end
+    end
  end
 
 items = [1, 2, 3, 4, 5, 6, 7]
@@ -61,5 +122,23 @@ items.each { |x| tree.insert(x) }
 
 tree.traverse { |x| print "#{x} " }
 print "\n"
+
+def addnode(nodes)
+    node = nodes.shift
+    tree = Tree.new node
+    if %w(* / + -).include? node
+        tree.left = addnode nodes
+        tree.right = addnode nodes
+    end
+    tree
+end
+
+prefix = %w( * + 32 * 21 45 - 72 + 23 11 )
+
+tree = addnode prefix
+
+str = ''
+tree.infix { |x| str += x }
+# str is now "(32+(21*45))*(72-(23+11))"
 
 # Prints "1 2 3 4 5 6 7 "
