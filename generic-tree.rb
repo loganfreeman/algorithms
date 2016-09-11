a7412022ad08108f670c28c9c8774fe9adb6150a
loganfreeman <shanhongcheng@hotmail.com>
2016-09-11 10:15:55 -0600

    Update generic-tree.rb

--- generic-tree.rb
+++ generic-tree.rb
@@ -1,47 +1,48 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+require 'pp'
 class LinkedList
-  # By implementing LinkedList#each, we can include Enumerable
-  include Enumerable
+    # By implementing LinkedList#each, we can include Enumerable
+    include Enumerable
 
-  attr_reader :head, :length
+    attr_reader :head, :length
 
-  def initialize
-    @head = LinkedListNode.new(nil)
-    @length = 0
-  end
-
-  # O(1) time
-  def unshift(value)
-    @head = @head.insert_before(value)
-    @length += 1
-
-    self
-  end
-
-  # O(1) time
-  def shift
-    result = @head.value
-    @head = LinkedListNode(@head.next)
-    @length = [length - 1, 0].max
-
-    result
-  end
-
-  # O(1) time
-  def empty?
-    self.head.empty?
-  end
-
-  def each(&block)
-    node = self.head
-
-    until node.empty?
-      block.call(node.value)
-      node = node.next
+    def initialize
+        @head = LinkedListNode.new(nil)
+        @length = 0
     end
 
-    self
-  end
+    # O(1) time
+    def unshift(value)
+        @head = @head.insert_before(value)
+        @length += 1
 
+        self
+    end
+
+    # O(1) time
+    def shift
+        result = @head.value
+        @head = LinkedListNode(@head.next)
+        @length = [length - 1, 0].max
+
+        result
+    end
+
+    # O(1) time
+    def empty?
+        head.empty?
+    end
+
+    def each
+        node = head
+
+        until node.empty?
+            yield(node.value)
+            node = node.next
+        end
+
+        self
+    end
 end
 
 # This allows us to call LinkedListNode(value) and is different
@@ -49,72 +50,106 @@ end
 # like Integer(value), Array(value), String(value), etc.
 # work.
 def LinkedListNode(value)
-  case value
-  when LinkedListNode
-    value
-  else
-    LinkedListNode.new(value)
-  end
+    case value
+    when LinkedListNode
+        value
+    else
+        LinkedListNode.new(value)
+    end
 end
 
 class LinkedListNode
-  attr_accessor :value, :next
+    attr_accessor :value, :next
 
-  def initialize(value = nil, next_node = nil)
-    @value = value
-    @next  = next_node
-  end
+    def initialize(value = nil, next_node = nil)
+        @value = value
+        @next = next_node
+    end
 
-  # O(1) time
-  # Insert +value+ after this LinkedListNode and return new LinkedListNode
-  def insert_after(value)
-    node = LinkedListNode(value)
+    # O(1) time
+    # Insert +value+ after this LinkedListNode and return new LinkedListNode
+    def insert_after(value)
+        node = LinkedListNode(value)
 
-    node.next = self.next if self.next
-    self.next = node
+        node.next = self.next if self.next
+        self.next = node
 
-    node
-  end
+        node
+    end
 
-  # O(1) time
-  # Insert +value+ before this LinkedListNode and return new LinkedListNode
-  def insert_before(value)
-    node = LinkedListNode(value)
+    # O(1) time
+    # Insert +value+ before this LinkedListNode and return new LinkedListNode
+    def insert_before(value)
+        node = LinkedListNode(value)
 
-    node.next = self
+        node.next = self
 
-    node
-  end
+        node
+    end
 
-  def empty?
-    value.nil?
-  end
+    def empty?
+        value.nil?
+    end
 end
 
 def Tree(value)
-  case value
-  when Tree
-    value
-  else
-    Tree.new(value)
-  end
+    case value
+    when Tree
+        value
+    else
+        Tree.new(value)
+    end
 end
 
 # Implement a generic tree class.  Each node
 # is also a Tree and can have any number of children.
 class Tree
-  attr_reader :value, :children
-  def initialize(value)
-    @value    = value
-    @children = LinkedList.new
-  end
+    attr_reader :value, :children
+    def initialize(value)
+        @value = value
+        @children = LinkedList.new
+    end
 
-  # Add a new child to this node
-  # O(1) time
-  def add_child(value)
-    @children.unshift(Tree(value))
-  end
+    # Add a new child to this node
+    # O(1) time
+    def add_child(value)
+        @children.unshift(Tree(value))
+    end
 
-  def each(&block)
+    def each(&block)
+    end
+
+    def traverse
+        list = []
+        yield @value
+        @children.each do |child|
+            list.push(child)
+        end
+
+        loop do
+            break if list.empty?
+            node = list.shift
+            yield node.value
+            node.children.each do |child|
+                list.push(child)
+            end
+        end
   end
+end
+
+N, M = gets.strip.split(' ').map(&:to_i)
+i = 1
+hash = {}
+i = 1
+while i <= N
+    hash[i] = Tree.new(i)
+    i += 1
+end
+i = 1
+while i <= M
+    from, to = gets.strip.split(' ').map(&:to_i).sort
+    parent = hash[from]
+    child = hash[to]
+    parent.add_child(child)
+    i += 1
 end
