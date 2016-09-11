8f8376e3838e6aadc77a0d66e5f89d0c51ee0715
loganfreeman <shanhongcheng@hotmail.com>
2016-09-11 09:37:45 -0600

    Create generic-tree.rb

---
+++ generic-tree.rb
@@ -0,0 +1,120 @@
+class LinkedList
+  # By implementing LinkedList#each, we can include Enumerable
+  include Enumerable
+
+  attr_reader :head, :length
+
+  def initialize
+    @head = LinkedListNode.new(nil)
+    @length = 0
+  end
+
+  # O(1) time
+  def unshift(value)
+    @head = @head.insert_before(value)
+    @length += 1
+
+    self
+  end
+
+  # O(1) time
+  def shift
+    result = @head.value
+    @head = LinkedListNode(@head.next)
+    @length = [length - 1, 0].max
+
+    result
+  end
+
+  # O(1) time
+  def empty?
+    self.head.empty?
+  end
+
+  def each(&block)
+    node = self.head
+
+    until node.empty?
+      block.call(node.value)
+      node = node.next
+    end
+
+    self
+  end
+
+end
+
+# This allows us to call LinkedListNode(value) and is different
+# than the class "LinkedListNode".  This is how Ruby methods
+# like Integer(value), Array(value), String(value), etc.
+# work.
+def LinkedListNode(value)
+  case value
+  when LinkedListNode
+    value
+  else
+    LinkedListNode.new(value)
+  end
+end
+
+class LinkedListNode
+  attr_accessor :value, :next
+
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next  = next_node
+  end
+
+  # O(1) time
+  # Insert +value+ after this LinkedListNode and return new LinkedListNode
+  def insert_after(value)
+    node = LinkedListNode(value)
+
+    node.next = self.next if self.next
+    self.next = node
+
+    node
+  end
+
+  # O(1) time
+  # Insert +value+ before this LinkedListNode and return new LinkedListNode
+  def insert_before(value)
+    node = LinkedListNode(value)
+
+    node.next = self
+
+    node
+  end
+
+  def empty?
+    value.nil?
+  end
+end
+
+def Tree(value)
+  case value
+  when Tree
+    value
+  else
+    Tree.new(value)
+  end
+end
+
+# Implement a generic tree class.  Each node
+# is also a Tree and can have any number of children.
+class Tree
+  attr_reader :value, :children
+  def initialize(value)
+    @value    = value
+    @children = LinkedList.new
+  end
+
+  # Add a new child to this node
+  # O(1) time
+  def add_child(value)
+    @children.unshift(Tree(value))
+  end
+
+  def each(&block)
+  end
+end
