6c65f66b3fcfd80ea2b88ddcc9e0a480c5a78149
loganfreeman <shanhongcheng@hotmail.com>
2016-09-12 12:03:16 -0600

    Update subsets.rb

--- subsets.rb
+++ subsets.rb
@@ -7,3 +7,23 @@ def subsets(arr)
   end
   old_subsets + new_subsets
 end
+
+
+def find_subset(input_array)
+    no_of_subsets = 2**input_array.length - 1
+    all_subsets = []
+    expected_length_of_binary_no = input_array.length
+    for i in 1..(no_of_subsets) do 
+        binary_string = i.to_s(2)
+        binary_string = binary_string.rjust(expected_length_of_binary_no, '0')
+        binary_array = binary_string.split('')
+        subset = []
+        binary_array.each_with_index do |bin, index|
+            if bin.to_i == 1
+                subset.push(input_array[index]) 
+            end
+        end
+        all_subsets.push(subset)
+    end
+    all_subsets
+end
