e5ae5af4717c21089de110dae005da0d2995e460
loganfreeman <shanhongcheng@hotmail.com>
2016-09-12 11:58:59 -0600

    Create subsets.rb

---
+++ subsets.rb
@@ -0,0 +1,9 @@
+def subsets(arr)
+  return [[]] if arr.empty?
+  old_subsets = subsets(arr.drop(1))
+  new_subsets = []
+  old_subsets.each do |subset|
+    new_subsets << subset + [arr.first]
+  end
+  old_subsets + new_subsets
+end
