bdb71861ba783d1ba4bca6a03a3028371f0d5bf5
loganfreeman <shanhongcheng@hotmail.com>
2016-09-13 13:41:40 -0600

    Update max-partition.rb

--- max-partition.rb
+++ max-partition.rb
@@ -1,13 +1,28 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 n = gets.strip.to_i
 
+def display_table(table)
+    table.each do |row| 
+        puts row.map {|i| i ? 1 : 0 }.join(' ')
+    end
+end
+
 def track(array, p, i, j, set = [])
-    if !p[i][j-1]
-        sum = i - array[j-1]
-        set.push(j-1)
-        track(array, p, sum, j-1, set)
-    else
-        track(array, p, i, j-1, set) if j > 1
+    
+        
+    begin
+        if !p[i][j-1]
+            sum = i - array[j-1]
+            set.push(j-1)
+            track(array, p, sum, j-1, set) if i >= array[j-1]
+        else
+            track(array, p, i, j-1, set) if j > 1
+
+        end
+    
+    rescue => exception
+        puts "***#{i} #{j-1}****"
+        raise exception
     end
     set
 end
@@ -20,19 +35,21 @@ def find_partition(array)
     if k % 2 == 1
         return [false, nil]
     end
-    rows = (k/2).floor + 1
+    rows = k/2 + 1
     p = Array.new(rows) { Array.new(n+1)}
+    
     (1..n+1).each { |i| p[0][i-1] = true }
     (1..rows).each { |i| p[i-1][0] = false }
     p[0][0] = true
-    (1..(k/2).floor).each do |i|
+    (1..(k/2)).each do |i|
         (1..n).each do |j|
-            p[i][j] = p[i][j-1] || p[i - array[j-1]][j-1]
+            p[i][j] = p[i][j-1]
+            p[i][j] = p[i][j-1] || p[i - array[j-1]][j-1] if i - array[j-1] >= 0
         end
         
     end
-    subset = track(array, p, (k/2).floor, n)
-    can_partition = p[(k/2).floor][n]
+    subset = track(array, p, (k/2), n)
+    can_partition = p[(k/2)][n]
     parts = []
     if can_partition
         left = []
