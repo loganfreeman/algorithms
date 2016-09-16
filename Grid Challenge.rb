06c4dbdd41f190d6d32f435658d4ec4ebdd59797
loganfreeman <shanhongcheng@hotmail.com>
2016-09-15 22:41:08 -0600

    Update Grid Challenge.rb

--- Grid Challenge.rb
+++ Grid Challenge.rb
@@ -15,6 +15,7 @@ def read
 end
 
 def display(grid)
+    puts '##########'
     grid.each do |row|
         puts row.join('')
     end
@@ -24,8 +25,30 @@ def swap(grid, i, j)
     grid[i][j], grid[i][j+1] = grid[i][j+1], grid[i][j]
 end
 
+def check_column(grid, col, size)
+    sorted = true
+    1.upto(size-1).each do |r|
+        if grid[r-1][col] > grid[r][col]
+            sorted = false
+        end
+    end
+    sorted
+end
+
 def solve(grid, size)
-    display(grid)
+    1.upto(size).each do |r|
+        row = grid[r-1]
+        row.sort!
+    end
+    # display(grid)
+    first = (1..size).find do |col|
+        !check_column(grid, col-1, size)
+    end
+    if !first.nil?
+        puts 'NO'
+    else
+        puts 'YES'
+    end
 end
 
 read
