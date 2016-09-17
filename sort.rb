0a332d4104f7821d314a35935d2c6dc51af9b574
loganfreeman <shanhongcheng@hotmail.com>
2016-09-17 10:08:24 -0600

    Update sort.rb

--- sort.rb
+++ sort.rb
@@ -1,4 +1,10 @@
 module Hacker
+    def self.quicksort(array)
+      return [] if array.empty? || array.nil?
+      pv = array.shift
+      quicksort(array.select { |e| e < pv }) + [pv] + quicksort(array.select { |e| e >= pv })
+    end
+
     def self.insertion_sort(array)
       1.upto(array.length - 1) do |i|
         x = array[i]
@@ -159,4 +165,63 @@ module Sortable
         swap(lo, j, array)
         j
     end
+end
+
+
+class QuickSort
+
+  # O(n) space complexity
+  def self.sort1(array)
+    return array if array.length <= 1
+
+    pivot_idx = rand(array.length)
+    array[0], array[pivot_idx] = array[pivot_idx], array[0]
+    pivot = array.first
+
+    array = array.drop(1)
+
+    left, right = [], []
+
+    array.each do |el|
+      el <= pivot ? left << el : right << el
+    end
+
+    sort1(left) + [pivot] + sort1(right)
+  end
+
+  # In-place [ O(1) space ]
+  def self.sort2!(array, start = 0, length = array.length, &prc)
+    prc ||= Proc.new { |el1, el2| el1 <=> el2 }
+    return array if length <= 1
+
+    pivot = self.partition(array, start, length, &prc)
+
+    left_length = pivot - start
+    right_length = length - (left_length + 1)
+
+    self.sort2!(array, start, left_length, &prc)
+    self.sort2!(array, pivot + 1, right_length, &prc)
+    array
+  end
+
+  def self.partition(array, start, length, &prc)
+    prc ||= Proc.new { |el1, el2| el1 <=> el2 }
+
+    pivot_idx = start + rand(length)
+    array[start], array[pivot_idx] = array[pivot_idx], array[start]
+    pivot = array[start]
+
+    (start + 1).upto(start + length - 1).each do |idx|
+
+      val = prc.call(array[idx], pivot)
+
+      if val <= 0
+        array[start + 1], array[idx] = array[idx], array[start + 1]
+        array[start], array[start + 1] = array[start + 1], array[start]
+        start += 1
+      end
+    end
+
+    return start
+  end
 end
